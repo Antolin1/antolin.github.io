5977a5356a9daffa14fcc74216a4aaf98de74b2b
antolin <joseantonio.hernandez6@um.es>
2020-08-02 18:10:37 +0200

    Updates

---
+++ precache-manifest.92f09103b127068232e9202b1c95673e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dea4a8a6d25bcc8e34fe9cf224644fd",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8d6058a1d906c25ea47f",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6058a1d906c25ea47f",
+    "url": "/home/static/js/main.e6348181.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/home/static/media/resume.0c246fa0.pdf"
+  }
+]);
